bf6e92d3882ca5d6a4c511851ea0d0ef96d4f925
shen.liao <shen.liao@thoughtworks.com>
2022-01-05 15:31:22 +0800

    feat: [#refactor1-4] move amountFor to index.js

--- index.js
+++ index.js
@@ -1,6 +1,29 @@
 const plays = require('./plays.json');
 const invoices = require('./invoices.json');
-const amountFor = require('./amountFor');
+
+const amountFor = (aPerformance, perf) => {
+    let result = 0;
+
+    switch (aPerformance.type) {
+        case "tragedy":
+            result = 40000;
+            if (perf.audience > 30) {
+                result += 1000 * (perf.audience - 30);
+            }
+            break;
+
+        case "comedy":
+            result = 30000;
+            if (perf.audience > 20) {
+                result += 10000 + 500 * (perf.audience - 20);
+            }
+            break;
+        default:
+            throw new Error(`unknown type: ${aPerformance.type}`)
+    }
+
+    return result;
+}
 
 function printInvoice(invoice, plays) {
     let totalAmount = 0;
