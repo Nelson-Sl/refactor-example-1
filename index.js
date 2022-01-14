3aa645b5cbd3d16971acdc879372863cdc67cc9b
shen.liao <shen.liao@thoughtworks.com>
2022-01-14 15:56:14 +0800

    feat: [#refactor1-6] add play information for each performance into statementData

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ const renderPlainText = (data, plays) => {
 
     for (let perf of data.performances) {
         // print line for each performance order
-        result += ` ${playFor(perf).name}: ${usd(amountFor(perf))} (#${perf.audience} seats)\n`;
+        result += ` ${perf.play.name}: ${usd(amountFor(perf))} (#${perf.audience} seats)\n`;
     }
 
     result += `Amount owed is ${usd(totalAmount())}\n`;
@@ -17,6 +17,28 @@ const renderPlainText = (data, plays) => {
         let result = 0;
         for (let perf of data.performances) {
             result += amountFor(perf);
+        }
+        return result;
+    }
+
+    function amountFor(aPerformance) {
+        let result = 0;
+        switch (aPerformance.play.type) {
+            case 'tragedy':
+                result = 40000;
+                if(aPerformance.audience > 30) {
+                    result += 1000 * (aPerformance.audience - 30);
+                }
+                break;
+            case 'comedy':
+                result = 30000;
+                if(aPerformance.audience > 20) {
+                    result += 10000 + 500 * (aPerformance.audience - 20);
+                }
+                result += 300 * aPerformance.audience;
+                break;
+            default:
+                throw new Error(`unknown type: ${aPerformance.play.type}`)
         }
         return result;
     }
@@ -34,7 +56,7 @@ const renderPlainText = (data, plays) => {
         //add volume credits
         result += Math.max(aPerformance.audience - 30, 0);
         //add extra credit for every ten comedy attendees
-        if ('comedy' === playFor(aPerformance).type) {
+        if ('comedy' === aPerformance.play.type) {
             result += Math.floor(aPerformance.audience / 5);
         }
         return result;
@@ -47,17 +69,25 @@ const renderPlainText = (data, plays) => {
             minimumFractionDigits: 2
         }).format(aNumber/100);
     }
+}
+
+function printInvoice(invoice, plays) {
+    // Statement Data: the structure of data needed in program
+    const statementData = {};
+    statementData.customer = invoice.customer;
+    statementData.performances = invoice.performances.map(enrichPerformance);
+    console.log(JSON.stringify(statementData));
+    return renderPlainText(statementData, plays);
+
+    function enrichPerformance(aPerformance) {
+        const result = Object.assign({}, aPerformance);
+        result.play = playFor(result);
+        return result;
+    }
 
     function playFor(aPerformance) {
         return plays[aPerformance.playID];
     }
-}
-
-function printInvoice(invoice, plays) {
-    const statementData = {};
-    statementData.customer = invoice.customer;
-    statementData.performances = invoice.performances;
-    return renderPlainText(statementData, plays);
 }
 
 console.log(printInvoice(invoices, plays));
