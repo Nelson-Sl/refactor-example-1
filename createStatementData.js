eff8ddb75e88176a14d56c81f86f36d646186855
shen.liao <shen.liao@thoughtworks.com>
2022-01-26 16:20:19 +0800

    feat: [#refactor1-7] create PerformanceCalculator to calculate performance data & add getAmount data

--- createStatementData.js
+++ createStatementData.js
@@ -1,3 +1,5 @@
+import {PerformanceCalculator} from "./PerformanceCalculator.js";
+
 export default function createStatementData(invoices, plays) {
     // Statement Data: the structure of data needed in program
     const statementData = {};
@@ -8,37 +10,16 @@ export default function createStatementData(invoices, plays) {
     return statementData;
 
     function enrichPerformance(aPerformance) {
+        const calculator = new PerformanceCalculator(aPerformance, playFor(aPerformance));
         const result = Object.assign({}, aPerformance);
         result.play = playFor(result);
-        result.amount = amountFor(result);
+        result.amount = calculator.getAmount();
         result.volumeCredits = volumeCreditsFor(result);
         return result;
     }
 
     function playFor(aPerformance) {
         return plays[aPerformance.playID];
-    }
-
-    function amountFor(aPerformance) {
-        let result = 0;
-        switch (aPerformance.play.type) {
-            case 'tragedy':
-                result = 40000;
-                if(aPerformance.audience > 30) {
-                    result += 1000 * (aPerformance.audience - 30);
-                }
-                break;
-            case 'comedy':
-                result = 30000;
-                if(aPerformance.audience > 20) {
-                    result += 10000 + 500 * (aPerformance.audience - 20);
-                }
-                result += 300 * aPerformance.audience;
-                break;
-            default:
-                throw new Error(`unknown type: ${aPerformance.play.type}`)
-        }
-        return result;
     }
 
     function volumeCreditsFor(aPerformance) {
